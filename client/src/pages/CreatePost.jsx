d762e85db4f88c9cc6e1872f4129bcf90cd0985d
BrimoRishabhm <rishabh.m@brimo.in>
2023-12-14 23:52:47 +0530

    new api key added

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -22,7 +22,7 @@ const CreatePost = () => {
     if (form.prompt) {
       try {
         setGeneratingImg(true);
-        const response = await fetch('http://localhost:8080/api/v1/dalle', {
+        const response = await fetch('https://ai-image-generator-z36i.onrender.com/api/v1/dalle', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
@@ -52,7 +52,7 @@ const CreatePost = () => {
     if (form.prompt && form.photo) {
       setLoading(true);
       try {
-        const response = await fetch('http://localhost:8080/api/v1/post', {
+        const response = await fetch('https://ai-image-generator-z36i.onrender.com/api/v1/post', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
